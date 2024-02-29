aa64544209a7db911122d8642211fc937c200657
Rutwik Jain <rnjain@c198-062.frontera.tacc.utexas.edu>
2024-02-28 22:56:34 -0600

    singularity setup added

--- blox_exp/scripts/run_imagenet.sh
+++ blox_exp/scripts/run_imagenet.sh
@@ -1,5 +1,9 @@
+module load tacc-apptainer
+
 export CUDA_VISIBLE_DEVICES=$1
-python $SCRATCH/Megatron-Resource/blox_exp/models/imagenet_ddp.py \
+
+singularity pull docker://nvcr.io/nvidia/pytorch:22.06-py3
+singularity exec --nv docker://nvcr.io/nvidia/pytorch:22.06-py3 python $SCRATCH/Megatron-Resource/blox_exp/models/imagenet_ddp.py \
     --master-ip-address=$2 \
     --world-size=$3 \
     --rank=$4 \
